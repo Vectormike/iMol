aa070d5ec90f91a3f1202076f00bca4c9917b18d
Vectormike <victorjonah199@gmail.com>
2019-10-10 15:53:32 +0100

    ...

--- server/server.js
+++ server/server.js
@@ -10,15 +10,13 @@ import morgan from "morgan";
 
 import { mongoURI } from "../server/config/db";
 
-import products from "./routes/api/products";
-
 const app = express();
 const port = 7000;
 
 const debug = createDebug("app");
 
 // Middlewares
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -31,7 +29,8 @@ mongoose
   .then(() => debug(`${chalk.green(`MongoDB Connected`)}`))
   .catch(error => debug(`Database is not connected... ${chalk.red(error)}`));
 
-// // Use Routes
-// app.use("/api/products/", products);
+// Use Routes
+import products from "./routes/api/products";
+app.use("/api/products/", products);
 
 app.listen(port, () => debug(`Server running on port ${chalk.green(port)}`));
