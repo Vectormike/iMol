432035e074cbae85957d2162c764a0b54d04588b
Vectormike <victorjonah199@gmail.com>
2019-10-10 15:54:27 +0100

    Added to model

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,9 +3,18 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const productSchema = new Schema({
-  name: String,
-  price: Number,
-  updatedAt: { type: Date, default: Date.now() }
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 export const Product = mongoose.model("Product", productSchema);
