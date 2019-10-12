bb645165de24bf4b7b43c578eed7095d627cb44d
Vectormike <victorjonah199@gmail.com>
2019-10-12 15:30:17 +0100

    Modified routes

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -2,54 +2,27 @@ import { Router } from "express";
 const router = Router();
 
 // Controllers
-import {
+import ProductControllers from "../../controllers/product";
+
+const {
+  getAllProducts,
+  addProduct,
   getProduct,
   editProduct,
   deleteProduct
-} from "../../controllers/product";
-
-// Model
-import { Product } from "../../models/Product";
+} = ProductControllers;
 
 // Routes
 
 // @route GET api/products
 // @desc Get all products
 // @access Public
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({
-      message: "No Products created yet..."
-    });
-  }
-});
+router.get("/", getAllProducts);
 
 // @route POST api/products
 // @desc Add a product
 // @access Public
-router.post("/", async (req, res) => {
-  const { name, price } = req.body;
-  const product = new Product({
-    name,
-    price
-  });
-  try {
-    const newProduct = await product.save();
-    if (newProduct) {
-      res.status(201).json(newProduct);
-    }
-    res.status(401).json({
-      message: "Couldn't add product..."
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
-  }
-});
+router.post("/", addProduct);
 
 // @route GET api/products/:id
 // @desc Search/Get a product
