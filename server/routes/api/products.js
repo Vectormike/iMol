11432c8bce7acdbf1f0a4008c6a7618b7696304f
Vectormike <victorjonah199@gmail.com>
2019-10-18 15:49:14 +0100

    Added auth to routes

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -4,6 +4,9 @@ const router = Router();
 // Controllers
 import ProductControllers from "../../controllers/product";
 
+// Middleware
+import { verifyToken } from "../../middlewares/jwt";
+
 const {
   getAllProducts,
   addProduct,
@@ -17,26 +20,26 @@ const {
 // @route GET api/products
 // @desc Get all products
 // @access Public
-router.get("/", getAllProducts);
+router.get("/", verifyToken, getAllProducts);
 
 // @route POST api/products
 // @desc Add a product
 // @access Public
-router.post("/", addProduct);
+router.post("/", verifyToken, addProduct);
 
 // @route GET api/products/:id
 // @desc Search/Get a product
 // @access Public
-router.get("/:id", getProduct);
+router.get("/:id", verifyToken, getProduct);
 
 // @route PATCH api/products
 // @desc Edit a product
 // @access Public
-router.patch("/:id", editProduct);
+router.patch("/:id", verifyToken, editProduct);
 
 // @route DELETE api/product/:id
 // @desc Delete a product
 // @access Public
-router.delete("/:id", deleteProduct);
+router.delete("/:id", verifyToken, deleteProduct);
 
 export default router;
