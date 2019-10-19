0646264d1ac3d3a5ff4c1dd530620a01fb66f3f0
Vectormike <victorjonah199@gmail.com>
2019-10-19 11:02:05 +0100

    Middleware

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 const router = Router();
 
+// Middlewares
+import UserMiddlewares from "../../middlewares/auth";
+const { validatePassword } = UserMiddlewares;
+
 // Controller
 import UserControllers from "../../controllers/auth";
-
 const { registerUser, loginUser } = UserControllers;
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post("/register", registerUser);
+router.post("/register", validatePassword, registerUser);
 
 // @route POST api/auth/login
 // @desc Auth a user
