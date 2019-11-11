69209f5a0e5e10d70e0e9bc12d1d1381018788a3
Vectormike <victorjonah199@gmail.com>
2019-11-12 00:17:53 +0100

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { ThemeProvider, CSSReset } from "chakra-ui/core";
+import React from 'react';
+import './App.css';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import Homepage from './pages/homepage/homepage';
 
 function App() {
   return (
     <ThemeProvider>
       <CSSReset />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <Homepage />
       </div>
     </ThemeProvider>
   );
