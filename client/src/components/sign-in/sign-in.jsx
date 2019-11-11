1f29bea2c065f45bde08fdeaf7ac36b9d075eff6
Vectormike <victorjonah199@gmail.com>
2019-11-12 00:17:46 +0100

    Login and register component

---
+++ client/src/components/sign-in/sign-in.jsx
@@ -0,0 +1,96 @@
+import React, { useRef, useState } from 'react';
+import { useDisclosure } from '@chakra-ui/core';
+import {
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Stack,
+  Box,
+  FormLabel,
+  Input,
+  InputRightElement,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Select,
+  Textarea
+} from '@chakra-ui/core';
+
+const SignIn = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const firstField = useRef();
+  const btnRef = useRef();
+
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        mx='auto'
+        leftIcon='add'
+        variantColor='teal'
+        size='sm'
+        variant='ghost'
+        onClick={onOpen}>
+        Sign In
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        initialFocusRef={firstField}
+        finalFocusRef={btnRef}
+        onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth='1px'>Sign In</DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing='24px'>
+              <Box>
+                <FormLabel htmlFor='username'>Email</FormLabel>
+                <Input
+                  ref={firstField}
+                  id='username'
+                  placeholder='Please enter email'
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor='username'>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    pr='4.5rem'
+                    type={show ? 'text' : 'password'}
+                    placeholder='Enter password'
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleClick}>
+                      {show ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth='1px'>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button variantColor='blue'>Submit</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ client/src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignUp from '../../components/sign-up/sign-up';
+import SignIn from '../../components/sign-in/sign-in';
+const Homepage = () => (
+  <div>
+    <SignUp />
+    <SignIn />
+  </div>
+);
+
+export default Homepage;
