9751538add77cfd9e5bd6b64c70007a15f0f2b7c
Vectormike <victorjonah199@gmail.com>
2019-11-12 14:46:26 +0100

    ...

--- client/src/components/sign-up/sign-up.jsx
+++ client/src/components/sign-up/sign-up.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { useDisclosure } from '@chakra-ui/core';
+import { useDisclosure, FormControl } from '@chakra-ui/core';
 import {
   Button,
   Drawer,
@@ -23,6 +23,25 @@ const SignUp = () => {
   const btnRef = useRef();
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: [e.target.value] });
+  };
+  const onSubmit = e => {
+    if (password !== password2) {
+      console.log(password);
+    } else {
+      console.log(name);
+    }
+  };
 
   return (
     <>
@@ -43,57 +62,88 @@ const SignUp = () => {
         finalFocusRef={btnRef}
         onClose={onClose}>
         <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader borderBottomWidth='1px'>
-            Create a new account
-          </DrawerHeader>
+        <form onSubmit={onSubmit}>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader borderBottomWidth='1px'>
+              Create a new account
+            </DrawerHeader>
 
-          <DrawerBody>
-            <Stack spacing='24px'>
-              <Box>
-                <FormLabel htmlFor='username'>Name</FormLabel>
-                <Input
-                  ref={firstField}
-                  id='username'
-                  placeholder='Please enter full name'
-                />
-              </Box>
-
-              <Box>
-                <FormLabel htmlFor='username'>Email</FormLabel>
-                <Input
-                  ref={firstField}
-                  id='username'
-                  placeholder='Please enter email'
-                />
-              </Box>
-
-              <Box>
-                <FormLabel htmlFor='username'>Password</FormLabel>
-                <InputGroup>
+            <DrawerBody>
+              <Stack spacing='24px'>
+                <Box>
+                  <FormLabel htmlFor='name'>Name</FormLabel>
                   <Input
-                    pr='4.5rem'
-                    type={show ? 'text' : 'password'}
-                    placeholder='Enter password'
+                    ref={firstField}
+                    id='name'
+                    type='text'
+                    name='name'
+                    value={name}
+                    placeholder='Name'
+                    onChange={onChange}
                   />
-                  <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={handleClick}>
-                      {show ? 'Hide' : 'Show'}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </Box>
-            </Stack>
-          </DrawerBody>
+                </Box>
 
-          <DrawerFooter borderTopWidth='1px'>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button variantColor='blue'>Submit</Button>
-          </DrawerFooter>
-        </DrawerContent>
+                <Box>
+                  <FormLabel htmlFor='username'>Email</FormLabel>
+                  <Input
+                    ref={firstField}
+                    id='email'
+                    type='email'
+                    name='email'
+                    value={email}
+                    placeholder='Email'
+                    onChange={onChange}
+                  />
+                </Box>
+
+                <Box>
+                  <FormLabel htmlFor='username'>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      pr='4.5rem'
+                      type={show ? 'text' : 'password'}
+                      placeholder='Password'
+                      name='password'
+                      value={password}
+                      onChange={onChange}
+                    />
+                    <InputRightElement width='4.5rem'>
+                      <Button h='1.75rem' size='sm' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </Box>
+
+                <Box>
+                  <FormLabel htmlFor='confirmpassword'>
+                    Confirm Password
+                  </FormLabel>
+                  <InputGroup>
+                    <Input
+                      pr='4.5rem'
+                      type={show ? 'text' : 'password'}
+                      placeholder='Confirm Password'
+                      name='password2'
+                      value={password2}
+                      onChange={onChange}
+                    />
+                  </InputGroup>
+                </Box>
+              </Stack>
+            </DrawerBody>
+
+            <DrawerFooter borderTopWidth='1px'>
+              <Button variant='outline' mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button type='submit' variantColor='blue'>
+                Submit
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </form>
       </Drawer>
     </>
   );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import reducers from './root-reducer';
+import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-export const store = createStore(reducers, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
