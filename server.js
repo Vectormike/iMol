533fa69e15a454aeab83b0f4012153ffa85c2c05
Vectormike <victorjonah199@gmail.com>
2019-11-16 04:16:22 +0100

    ...

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-import express from "express";
+import express from 'express';
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import chalk from "chalk";
+import chalk from 'chalk';
 
-import createDebug from "debug";
+import createDebug from 'debug';
 
-import morgan from "morgan";
+import morgan from 'morgan';
 
-import { mongoURI } from "./config/db";
-import { cloudinaryConfig } from "./config/cloudinary";
+import { mongoURI } from './server/config/db';
+import { cloudinaryConfig } from './server/config/cloudinary';
 
 const app = express();
 const port = 7000;
 
-const debug = createDebug("app");
+const debug = createDebug('app');
 
 // Middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", cloudinaryConfig);
+app.use('/', cloudinaryConfig);
 
 // Connect to DB
 mongoose
@@ -34,11 +34,11 @@ mongoose
   .catch(error => debug(`Database is not connected... ${chalk.red(error)}`));
 
 // Use Routes
-import products from "./routes/api/products";
-import users from "./routes/api/auth";
-import uploads from "./routes/api/uploads";
-app.use("/api/products/", products);
-app.use("/api/auth/", users);
-app.use("/api/uploads/", uploads);
+import products from './server/routes/api/products';
+import users from './server/routes/api/auth';
+import uploads from './server/routes/api/uploads';
+app.use('/api/products/', products);
+app.use('/api/auth/', users);
+app.use('/api/uploads/', uploads);
 
 app.listen(port, () => debug(`Server running on port ${chalk.green(port)}`));
