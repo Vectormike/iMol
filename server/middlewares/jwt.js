95ab51a3b63f5e06f0538bc0229f629eab9b0d4e
Vectormike <victorjonah199@gmail.com>
2019-11-16 04:16:34 +0100

    ...

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -1,20 +1,20 @@
-import jwt from "jsonwebtoken";
-import { jwtSecret } from "../config/jwt";
+import jwt from 'jsonwebtoken';
+import { jwtSecret } from '../config/jwt';
 
 export const verifyToken = (req, res, next) => {
   try {
-    const token = req.header("x-auth-token");
+    const token = req.header('x-auth-token');
 
     if (!token) {
       return res.status(401).json({
-        message: "Authorization denied"
+        message: 'Authorization denied'
       });
     }
     const decoded = jwt.verify(token, jwtSecret);
     req.user = decoded;
   } catch (error) {
     res.status(500).json({
-      message: "Token is invalid"
+      message: 'Token is invalid'
     });
   }
   next();
