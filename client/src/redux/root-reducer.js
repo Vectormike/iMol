4788f8e34780123c3dc1f32fd2929cd1023973ae
Vectormike <victorjonah199@gmail.com>
2019-11-16 04:17:24 +0100

    ...

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import { alertReducer } from './reducers/alert';
+import { authReducer } from './reducers/auth';
 
 // Reducers
-const rootReducer = combineReducers({ alert: alertReducer });
+const rootReducer = combineReducers({ alert: alertReducer, auth: authReducer });
 
 export default rootReducer;
