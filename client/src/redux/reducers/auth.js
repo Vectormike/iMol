798b9de6aa7c742c635536f4b1c0934d677993c4
Vectormike <victorjonah199@gmail.com>
2019-11-17 20:17:28 +0100

    Failed registration state

--- client/src/redux/reducers/auth.js
+++ client/src/redux/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -17,7 +17,13 @@ export const authReducer = (state = initialState, action) => {
         ...payload,
         isAuthenticated: true
       };
-
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: null
+      };
     default:
       return state;
   }
