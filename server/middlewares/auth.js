027407510ca305e0b69c595a7b1e0423cf24a258
Vectormike <victorjonah199@gmail.com>
2019-11-17 20:17:47 +0100

    Fix

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,42 +7,35 @@ class UserMiddlewares {
       if (nameRegEx.test(name)) {
         throw new Error();
       }
-      next();
+      return next();
     } catch (error) {
       return res.status(400).json({
-        message: "Enter a name without digits"
+        message: 'Enter a name without digits'
       });
     }
   }
 
   static validateEmail(req, res, next) {
-    try {
-      const { email } = req.body;
-      const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { email } = req.body;
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-      if (!emailRegEx.test(email)) {
-        throw new Error();
-      }
-      next();
-    } catch (error) {
+    if (!emailRegEx.test(email)) {
       return res.status(400).json({
-        message: "Enter a valid email address"
+        message: 'Enter a valid email address'
       });
     }
+    return next();
   }
 
   static validatePassword(req, res, next) {
-    try {
-      const { password } = req.body;
-      if (password.length < 6) {
-        throw new Error();
-      }
-      next();
-    } catch (error) {
+    const { password } = req.body;
+
+    if (password.length < 6) {
       return res.status(400).json({
-        message: "Passwords must be more than 6 characters"
+        message: 'Password must be more than 6 characters'
       });
     }
+    return next();
   }
 }
 
