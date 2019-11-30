0e6dd1e6887641e2490b6c4cb97a4808d811111d
Vectormike <victorjonah199@gmail.com>
2019-11-30 02:43:43 +0100

    Get user details & login user

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -1,7 +1,34 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  USER_LOADED,
+  LOADED_FAIL
+} from './types';
 import { showAlert } from './alert';
+import setAuthToken from '../../helpers/helpers';
 
+// Get user details
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  const options = {
+    method: 'get',
+    url: 'http://localhost:7000/api/auth/'
+  };
+
+  try {
+    const response = await axios(options);
+    dispatch({ type: USER_LOADED, payload: response.data });
+  } catch (error) {
+    dispatch({ type: LOADED_FAIL });
+  }
+};
+
+// Register user
 export const register = ({ name, email, password }) => async dispatch => {
   let Name = name.toString();
   let Email = email.toString();
@@ -24,4 +51,27 @@ export const register = ({ name, email, password }) => async dispatch => {
     dispatch(showAlert(error.response.data.message, 'error', 5000));
   }
   dispatch({ type: REGISTER_FAIL });
+};
+
+// Loginn user
+export const login = ({ email, password }) => async dispatch => {
+  let Email = email.toString();
+  let Password = password.toString();
+
+  const options = {
+    method: 'post',
+    url: 'http://localhost:7000/api/auth/login',
+    data: {
+      email: Email,
+      password: Password
+    }
+  };
+  try {
+    const response = await axios(options);
+    dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+    dispatch(showAlert(response.data.message, 'success', 5000));
+  } catch (error) {
+    dispatch(showAlert(error.response.data.message, 'error', 5000));
+  }
+  dispatch({ type: LOGIN_FAIL });
 };
