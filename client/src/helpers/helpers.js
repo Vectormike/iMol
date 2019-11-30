7f8595cce4df1c34a4f16f95de840bf7206eb7b1
Vectormike <victorjonah199@gmail.com>
2019-11-30 02:43:16 +0100

    Set token to header

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -1,3 +1,13 @@
+import axios from 'axios';
+
 export const removeAlert = (state, payload) => {
   return state.filter(alert => alert.id !== payload);
+};
+
+export const setAuthToken = token => {
+  if (!token) {
+    delete axios.defaults.headers.common['x-auth-token'];
+  } else {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  }
 };
