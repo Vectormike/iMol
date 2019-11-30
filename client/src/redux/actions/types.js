2ebef452386c493b5db7392f4486783192a2433e
Vectormike <victorjonah199@gmail.com>
2019-11-30 02:44:09 +0100

    ...

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -2,3 +2,7 @@ export const SHOW_ALERT = 'SHOW_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const LOADED_FAIL = 'LOADED_FAIL';
