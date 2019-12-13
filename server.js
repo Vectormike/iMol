520c73b64e4601555eaf81ddf5aebbdb21615da0
Vectormike <victorjonah199@gmail.com>
2019-12-13 06:12:28 +0100

    Restructured app.js

--- server.js
+++ server.js
@@ -1,47 +1,15 @@
-import express from 'express';
-
-import mongoose from 'mongoose';
-import cors from 'cors';
+import app from './server/app';
 import chalk from 'chalk';
 
 import createDebug from 'debug';
 
 import morgan from 'morgan';
 
-import { mongoURI } from './server/config/db';
-import { cloudinaryConfig } from './server/config/cloudinary';
-
-const app = express();
-const port = 7000;
-
-app.use(cors());
 const debug = createDebug('app');
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.use('/', cloudinaryConfig);
-
-// Connect to DB
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(() => debug(`${chalk.green(`MongoDB Connected`)}`))
-  .catch(error => debug(`Database is not connected... ${chalk.red(error)}`));
-
-// Use Routes
-import products from './server/routes/api/products';
-import users from './server/routes/api/auth';
-import uploads from './server/routes/api/uploads';
-app.use('/api/products/', products);
-app.use('/api/auth/', users);
-app.use('/api/uploads/', uploads);
+const port = 7000;
 
 app.listen(port, () => debug(`Server running on port ${chalk.green(port)}`));
-
-export default app;
