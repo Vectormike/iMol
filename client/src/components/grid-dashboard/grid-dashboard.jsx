d70ca6a58e58b21c37307d8582e5aff5754b94d1
Vectormike <victorjonah199@gmail.com>
2019-12-16 22:53:11 +0100

    Dashboard panel

---
+++ client/src/components/grid-dashboard/grid-dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GridDashboard = () => {
+  return (
+    <section className='container'>
+      <div className='row'>
+        <div className='col-md-4 col-sm-4'>Dashboard</div>
+        <div className='col-md-8 col-sm-8'>View</div>
+      </div>
+    </section>
+  );
+};
+export default GridDashboard;
