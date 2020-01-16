26bace8a603ff30e9fbec4b64a4497bb36b60a14
Vectormike <victorjonah199@gmail.com>
2020-01-16 21:13:41 +0100

    ...

--- client/src/components/menu-view/menu-view.jsx
+++ client/src/components/menu-view/menu-view.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import Products from '../products/products';
 import Title from '../title/title';
 
 import Styles from '../../styles/menu-view.module.scss';
