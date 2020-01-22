c4bf7b7a79a88b50d8af44f8fd894f0d24b53e35
Vectormike <victorjonah199@gmail.com>
2020-01-22 12:42:40 +0100

    Added product reducer

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import { alertReducer } from './reducers/alert';
 import { authReducer } from './reducers/auth';
+import { productsReducer } from './reducers/product';
 
 // Reducers
-const rootReducer = combineReducers({ alert: alertReducer, auth: authReducer });
+const rootReducer = combineReducers({
+  alert: alertReducer,
+  auth: authReducer,
+  product: productsReducer
+});
 
 export default rootReducer;
