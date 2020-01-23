b15e136d76805a8f57de2e9d17d78afcc16dac5c
Vectormike <victorjonah199@gmail.com>
2020-01-23 16:54:02 +0100

    ...

--- client/src/components/table/table.jsx
+++ client/src/components/table/table.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getProducts } from '../../redux/actions/product';
@@ -8,15 +8,19 @@ import TableRow from './table-row';
 
 const header = ['Name', 'Price'];
 
-const Table = ({ product: products, loading }) => {
+const Table = ({ getProducts }) => {
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
   return (
     <Fragment>
       <table>
         <TableHeader header={header} />
         <tbody>
-          {products.map(product => (
-            <TableRow key={product._id} products={product} />
-          ))}
+          {/* {products.map((product, i) => (
+            <TableRow key={i} products={product} />
+          ))} */}
+          <TableRow />
         </tbody>
       </table>
     </Fragment>
@@ -24,7 +28,7 @@ const Table = ({ product: products, loading }) => {
 };
 
 Table.propTypes = {
-  product: PropTypes.object.isRequired,
+  product: PropTypes.isRequired,
   getProducts: PropTypes.func.isRequired
 };
 
