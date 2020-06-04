bae26343662f2a574b085d08174929dc93f67bf9
Redeemer40 <victorjonah199@gmail.com>
2020-06-04 09:34:38 +0100

    ...

---
+++ server/api/routes/uploads.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+const router = Router();
+
+// Middleware
+import { upload } from '../middlewares/multer';
+
+// Controller
+import UploadController from '../controllers/upload';
+const { uploadImage } = UploadController;
+
+// @route POST api/uploads/photo
+// @desc Upload a pgoto
+// @access Public
+router.post('/', upload, uploadImage);
+
+export default router;
