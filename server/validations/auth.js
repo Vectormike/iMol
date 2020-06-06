cb8d9268783d24621d02b445f922c0ee0becb542
Redeemer40 <victorjonah199@gmail.com>
2020-06-06 21:59:27 +0100

    ...

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-undef */
-import Joi from 'joi';
+import { Joi } from 'express-validation';
 
-exports = {
-  register: {
-    body: {
-      email: Joi.string().email().required(),
-      name: Joi.string().required(),
-      password: Joi.string().required().min(6).max(126),
-    },
+const register = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    name: Joi.string().required(),
+    password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required()
+      .min(6)
+      .max(126),
+  }),
+};
+
+const login = {
+  body: {
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required()
+      .min(6)
+      .max(126),
   },
 };
+
+export { register, login };
